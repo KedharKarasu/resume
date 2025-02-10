ac02322fa615a998186432880b58902ca222074c
KedharKarasu <karasukedhar@gmail.com>
2025-02-10 23:41:37 +0530

    second commit

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Portfolio from './Portfolio'; // Ensure Portfolio.js is also in src
+import Portfolio from './Portfolio.js'; // Ensure Portfolio.js is also in src
 
 function App() {
   return <Portfolio />;
